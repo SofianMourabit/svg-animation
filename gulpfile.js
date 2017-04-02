604755f3cf630081bcaa74bbed9cc47263df35df
Sofian Mourabit <smourabit@informationmapping.com>
2017-04-02 14:49:46 +0700

    update project to use svg sprite

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,117 @@
 'use strict';
 
-var gulp  = require('gulp'),
-server = require('gulp-server-livereload'),
-watch = require('gulp-watch'),
+const gulp = require('gulp'),
 sass = require('gulp-sass'),
-sourcemaps = require('gulp-sourcemaps');
+sourcemaps = require('gulp-sourcemaps'),
+babel = require("gulp-babel"),
+concat = require("gulp-concat"),
+autoprefixer = require('gulp-autoprefixer'),
+jshint = require('gulp-jshint'),
+notify = require('gulp-notify'),
+server = require('gulp-server-livereload'),
+del = require('del'),
+svgSprite = require('gulp-svg-sprite');
 
-gulp.task('compileSass', function () {
- return gulp.src('scss/main.scss')
+// Styles
+gulp.task('styles', function() {
+  return gulp.src('src/scss/application.scss')
   .pipe(sourcemaps.init())
   .pipe(sass())
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
   .pipe(sourcemaps.write('./'))
-  .pipe(gulp.dest('css'));
+  .pipe(gulp.dest('dist/css'))
+  .pipe(notify({ message: 'Styles task complete' }));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('scss/**/*.scss', ['compileSass']);
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('src/js/**/*.js')
+  .pipe(sourcemaps.init())
+  .pipe(babel())
+  .pipe(concat("app.js"))
+  .pipe(sourcemaps.write("."))
+  .pipe(jshint('.jshintrc'))
+  .pipe(jshint.reporter('default'))
+  .pipe(gulp.dest('dist/js'))
+  .pipe(notify({ message: 'Scripts task complete' }));
 });
 
-gulp.task('webserver',['compileSass','sass:watch'], function() {
-  gulp.src('./')
+// SVG icons CONFIG
+let config = {
+  mode: {
+    symbol: { // symbol mode to build the SVG
+      dest: 'dist/images/sprite', // destination foldeer
+      sprite: 'sprite.svg', //sprite name
+      example: true // Build sample page
+    }
+  },
+  svg: {
+    xmlDeclaration: false, // strip out the XML attribute
+    doctypeDeclaration: false // don't include the !DOCTYPE declaration
+  }
+};
+
+// Images
+gulp.task('sprite-page', function() {
+  return gulp.src('src/images/**/*.svg')
+  .pipe(svgSprite(config))
+  .pipe(gulp.dest('./'))
+  .pipe(notify({ message: 'Sprite page complete' }));
+});
+
+gulp.task('sprite-shortcut', function() {
+  return gulp.src('sprite/sprite.svg')
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/images/*')
+  .pipe(gulp.dest('dist/images'))
+  .pipe(notify({ message: 'Images task complete' }));
+});
+
+// Clean
+gulp.task('clean', function() {
+  return del(['dist/css', 'dist/js', 'dist/images','dist/images/sprite']);
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+  gulp.start('server');
+});
+
+
+// Watch
+gulp.task('watch', function () {
+  // Watch .scss files
+  gulp.watch('src/scss/**/*.scss', ['styles']);
+
+  // Watch .js files
+  gulp.watch('src/js/**/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('src/images/*', ['images']);
+
+  // Watch SVG files
+  gulp.watch('src/images/icons/*', ['sprite-page']);
+
+});
+
+//server
+gulp.task('server',['styles','scripts','images','sprite-page','watch'], function() {
+  gulp.src('./dist')
   .pipe(server({
     defaultFile: 'index.html',
     port: 3000,
     livereload: {
-        enable: true,
-        filter: function (filename, cb) {
-          cb(!/\.(sa|le)ss$|node_modules/.test(filename));
-        }
-      },
+      enable: true,
+      filter: function (filename, cb) {
+        cb(!/\.(sa|le)ss$|node_modules/.test(filename));
+      }
+    },
     open: true
   }));
 });
